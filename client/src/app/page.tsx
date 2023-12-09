fc5025037d6d7c818585fab8b2c861ee8b41959f
Szár Máté <smate6622@gmail.com>
2023-12-09 13:43:39 +0100

    Initial commit from Create Next App and initiate server

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,35 +6,40 @@ import { Card, Form, Container, Image, Button } from "react-bootstrap";
 export default function Home() {
   return (
     <html lang="hu">
-      <head>
-      <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet"/>
-      </head>
-      <body>
-        <nav>
-          
-          <h1>THRIVE TRIBE</h1>
+    <head>
+        <link rel="stylesheet" href="globals.css"></link>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
+        <title>Thrive Tribe</title>
+    </head>
+    <body>
 
-          <div className="nav-icon">
-              <a href="#">
-                <i className='bx bx-user'></i>
-              </a>
+    <nav>
+        <div className="logo">
+        <a>THRIVE TRIBE</a>
           </div>
 
-          <ul className="nav-links">
-            <li className="link"><a href="#">Étrend</a></li>
-            <li className="link"><a href="#">Edzés</a></li>
-            <li className="link"><a href="#">Edző</a></li>
-            <li className="link"><a href="#">Térkép</a></li>
-            <li className="link"><a href="#">Üzenőfal</a></li>
+          <ul>
+              <ul className="Menulinks">
+                  <li className="link"><a href="#">Étrend</a></li>
+                  <li className="link"><a href="#">Edzés</a></li>
+                  <li className="link"><a href="#">Edző</a></li>
+                  <li className="link"><a href="#">Térkép</a></li>
+                  <li className="link"><a href="#">Üzenőfal</a></li>
+              </ul>
           </ul>
 
-        </nav>
-        <img className="mainP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzVOObEkdf2ClOza_WLMSt9DF1KKthpdQE5g&usqp=CAU"></img>
-        <hr></hr>
+          <button id="userIcon">
+              <i className='bx bx-user'></i>
+          </button>
+      </nav>
+      <img className="mainP"
+           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzVOObEkdf2ClOza_WLMSt9DF1KKthpdQE5g&usqp=CAU"></img>
+      <img className="mainImagine" src="/client/src/pictures/duck-dance.gif"></img>
 
-        
-        <h2>Cikkek</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu consequat velit. Morbi viverra efficitur lectus ac vestibulum. Pellentesque volutpat egestas ligula, id tempus augue ultricies vitae. Aliquam ac magna sit amet velit luctus fermentum. Duis aliquam ullamcorper eros, quis porttitor ante scelerisque eget. Nunc vitae purus eu ex tempor imperdiet. Maecenas id nulla odio. Praesent ut magna iaculis, tristique sem sed, viverra sapien. Curabitur vel laoreet massa, sed volutpat mauris. Aliquam eget elit in leo dictum vestibulum ut vel felis. Suspendisse eget turpis non nibh malesuada fermentum elementum at libero. Proin risus dui, laoreet nec auctor nec, rutrum et odio. Vivamus ut ligula condimentum, sagittis lacus eget, vestibulum felis. Aenean pulvinar congue justo, quis ultricies velit suscipit sed.</p>
+      <hr></hr>
+
+      <h2 className="Articles" id="Articles">Cikkek</h2>
+
       </body>
     </html>
   )
