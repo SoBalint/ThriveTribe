ec5f7d073d8de3a6d8f249996021bbccca7b5958
Szár Máté <smate6622@gmail.com>
2024-01-24 08:34:05 +0100

    main Page fénykép

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,56 +5,60 @@ import React from "react";
 
 export default function Home() {
     return (
-        <Row>
-            <div className="mainPictures1">
-                <Image className="mainPictureGroup" src="/pictures/Image1.jpg" alt={"MainKép"}/>
-                <Image className="mainPictureMovement" src="/pictures/chrisheria.jpg" alt={"MainKép"}/>
-            </div>
 
+
+        <Row>
+            <div className="image-container">
+                <div className="overlay-text">Ép test ép lélek</div>
+                <Image className="mainPageImg" src="/pictures/chrisheria.jpg" alt={"MainKép"}/>
+            </div>
+            <div className="separatorLine"></div>
+    <Container>
+
+            <Col xs={12} className="articles" id="articles">
+                <h5>Cikkek</h5>
+                <div className="articlesMain" id="articlesMain">
+                    <div className="mainArt" id="mainArt">
+                        <div className="iconArticle" id="iconArticle"><i className='bx bx-dumbbell'></i></div>
+                        <h4>Workouts</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
+                        </p>
+                    </div>
+                    <div className="mainArt" id="mainArt">
+                        <div className="iconArticle" id="iconArticle">
+                            <i className='bx bx-bowl-hot'></i>
+                        </div>
+                        <h4>About Eating</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
+                            lectus dictum fermentum rhoncus, lacus nulla egestas neque,
+                        </p>
+                    </div>
+                    <div className="mainArt" id="mainArt">
+                        <div className="iconArticle" id="iconArticle"><i className='bx bx-info-square'></i></div>
+                        <h4>Important Thing</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
+                            lectus dictum fermentum rhoncus, lacus nulla egestas neque,
+                        </p>
+                    </div>
+                    <div className="mainArt" id="mainArt">
+                        <div className="iconArticle" id="iconArticle"><i className='bx bx-bell'></i></div>
+                        <h4>Santa Claus is HERE</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
+                            lectus dictum fermentum rhoncus, lacus nulla egestas neque,
+                        </p>
+                    </div>
+                </div>
+            </Col>
 
             <div className="separatorLine"></div>
 
-
-            <Col xs={12} className="articles" id="articles">
-        <h5>Cikkek</h5>
-        <div className="articlesMain" id="articlesMain">
-            <div className="mainArt" id="mainArt">
-                <div className="iconArticle" id="iconArticle"><i className='bx bx-dumbbell'></i></div>
-                <h4>Workouts</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
-                </p>
-            </div>
-            <div className="mainArt" id="mainArt">
-                <div className="iconArticle" id="iconArticle">
-                    <i className='bx bx-bowl-hot'></i>
-                </div>
-                <h4>About Eating</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
-                    lectus dictum fermentum rhoncus, lacus nulla egestas neque,
-                </p>
-            </div>
-            <div className="mainArt" id="mainArt">
-                <div className="iconArticle" id="iconArticle"><i className='bx bx-info-square'></i></div>
-                <h4>Important Thing</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
-                    lectus dictum fermentum rhoncus, lacus nulla egestas neque,
-                </p>
-            </div>
-            <div className="mainArt" id="mainArt">
-                <div className="iconArticle" id="iconArticle"><i className='bx bx-bell'></i></div>
-                <h4>Santa Claus is HERE</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta,
-                    lectus dictum fermentum rhoncus, lacus nulla egestas neque,
-                </p>
-            </div>
-        </div>
-    </Col>
-
-    <div className="separatorLine"></div>
+    </Container>
         </Row>
+
 )
+
 }
